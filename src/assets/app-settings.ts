152eb25f2209572e63c1aae9583d55d4c60f137a
Antonio <fagottino@users.noreply.github.com>
2019-05-23 16:35:48 +0200

    added constant

--- src/assets/app-settings.ts
+++ src/assets/app-settings.ts
@@ -3,4 +3,10 @@ export class AppSettings {
     public static WORK_EXPERIENCES: string = "workExperiences";
     public static EDUCATIONS: string = "educations";
     public static KNOWLEDGES: string = "knowledges";
+    public static CMS: string = "cms";
+    public static LANGUAGES: string = "languages";
+    public static SKILLandCOMPETENCES: string = "skillsAndCompetences";
+    public static INTERESTSandHOBBIES: string = "interestsAndHobbies";
+    public static PORTFOLIO: string = "portfolio";
+    public static CONTACTS: string = "contacts";
 }
